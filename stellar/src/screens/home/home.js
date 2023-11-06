06a4b4120277f29c241b09cc7e71c94c5397d83d
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-11-06 16:25:16 +0530

    Add files via upload

---
+++ stellar/src/screens/home/home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Library from '../library/library'
+import Feed from '../feed/feed'
+import Trending from '../trending/trending'
+import Player from '../player/player'
+import Favorites from '../favorites/favorites'
+import './home.css'
+
+export default function Home() {
+  return (
+
+    <Router>
+      <div className='main-body'>
+        <Routes>
+            <Route path='/' element={<Library />} />
+            <Route path='/feed' element={<Feed />} />
+            <Route path='/trending' element={<Trending />} />
+            <Route path='/player' element={<Player />} />
+            <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </div>  
+    </Router>
+  )
+}
